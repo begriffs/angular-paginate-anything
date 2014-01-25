855652565e61cf929f66b3926fe8a1019b8a1e7a
Joe Nelson <cred+github@begriffs.com>
2014-01-24 19:24:40 -0800

    Include $httpBackend in tests

--- test/existence_spec.js
+++ test/existence_spec.js
@@ -1,5 +1,11 @@
-define(['../paginated-resource.js'], function (presource) {
+define(['../paginated-resource.js', 'angular-mocks'], function (presource, mock) {
   'use strict';
+
+  var $httpBackend;
+  beforeEach(mock.inject(['$httpBackend', function(httpBackend){
+    $httpBackend = httpBackend;
+  }]));
+
   describe('paginated-resource', function () {
     it('exists', function () {
       return expect(presource.name).toBe('begriffs.paginated-resource');
